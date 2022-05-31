1f303e98853781ea42ea753e0b5455bd5e49b3e4
Henrik Stene <k01211@norsk-tipping.no>
2022-05-31 21:27:36 +0200

    Added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,41 @@
 import { fileURLToPath, URL } from 'url';
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'Desinsafe',
+          short_name: 'Desinsafe',
+          description: 'Desinsafe controller application',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'img/icon-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'img/icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'img/icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            }
+          ]
+        }
+      })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
